069eb69e96836760b7c70e0b0efac1eb34b70a91
Scott Davis <stdavis@utah.gov>
2019-03-22 21:04:59 -0600

    feat: add tag lines

--- src/App.js
+++ src/App.js
@@ -5,24 +5,55 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Jumbotron from 'react-bootstrap/Jumbotron';
-import Media from 'react-bootstrap/Media';
 
 
 class App extends Component {
   render() {
     return (
       <Container>
+        <Jumbotron>
+          <Row>
+            <Col>
+              <img src={logo} alt="logo" />
+            </Col>
+          </Row>
+        </Jumbotron>
         <Row>
           <Col>
-            <Jumbotron>
-              <Media>
-                <img src={logo} className="App-logo" alt="logo" />
-                <Media.Body>
-                  <h1>Because petroleum is for your car, not your skin.</h1>
-                  <p>We make skin care products that amaze our customers and are customized to their skin types.</p>
-                </Media.Body>
-              </Media>
-            </Jumbotron>
+            <Row>
+              <Col>
+                <h3>We're Simple</h3>
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <p>We enhance our lotions and creams only with essential oils. They don't contain any artificial colors or scents.</p>
+              </Col>
+            </Row>
+          </Col>
+          <Col>
+            <Row>
+              <Col>
+                <h3>We're Basic</h3>
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <p>Let mother nature pamper yoour skin with plant based oils like olive and avocado.</p>
+              </Col>
+            </Row>
+          </Col>
+          <Col>
+            <Row>
+              <Col>
+                <h3>We're Effective</h3>
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <p>Our products are petroleum free which allows your skin to breath and thrive. Because petroleum is for yoour car, not your skin.</p>
+              </Col>
+            </Row>
           </Col>
         </Row>
       </Container>
